75ade44f31961dcd0acb05c490d37b2a2c70e389
juan fricke <juanpfricke@outlook.com>
2023-07-19 01:23:43 -0300

    Creating registers

---
+++ include/chip8registers.h
@@ -0,0 +1,14 @@
+#ifndef CHIP8REGISTERS_H
+#define CHIP8REGISTERS_H
+
+
+#include "config.h"
+struct chip8_registers
+{
+    unsigned char V[C8_DATA_REGISTERS];
+};
+
+
+
+
+#endif
--- include/config.h
+++ include/config.h
@@ -6,5 +6,6 @@
 #define C8_WIDTH 64
 #define C8_HEIGHT 32
 #define WINDOW_SCALE 10
+#define C8_DATA_REGISTERS 16
 
 #endif
